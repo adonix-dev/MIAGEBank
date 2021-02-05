b02ae4c26052013fbc9e0dbe5c3561990a8557a3
Antony <antony-adonix-dev@users.noreply.github.com>
2021-02-05 14:05:40 +0100

    Inital Commit

---
+++ MIAGEBank-ejb/target/generated-sources/annotations/fr/miage/toulouse/entities/Operation_.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-02-04T11:31:32")
+@StaticMetamodel(Operation.class)
+public class Operation_ { 
+
+    public static volatile SingularAttribute<Operation, Long> id;
+
+}
